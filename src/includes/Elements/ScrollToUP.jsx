473a927bd27cb8a07ccf451d541eab288fdc4805
Horma-mohamed <96146244+Horma-mohamed@users.noreply.github.com>
2022-09-01 12:06:34 +0100

    first commit

--- src/includes/Elements/ScrollToUP.jsx
+++ src/includes/Elements/ScrollToUP.jsx
@@ -10,7 +10,7 @@ export default function ScrollToUp(){
         }
     })
     return(
-        <motion.div onClick={()=>{window.scrollTo({top:0,behavior:'smooth'})}} initial={{opacity:0,y:-10}} animate={control} className="md:px-4  p-2 md:py-2 border cursor-pointer flex justify-center md:space-x-2 items-center hover:text-white hover:bg-orange-400 transform duration-200 rounded-full border-orange-400 text-orange-400 fixed bottom-10 right-10 z-[99999999]">
+        <motion.div onClick={()=>{window.scrollTo({top:0,behavior:'smooth'})}} initial={{opacity:0,y:-10}} animate={control} className="md:px-4  p-3 md:py-2 border cursor-pointer flex justify-center md:space-x-2 items-center hover:text-white hover:bg-orange-400 transform duration-200 rounded-full border-orange-400 text-orange-400 fixed bottom-10 right-10 z-[99999999]">
                <span className="md:block hidden"> Scroll To The Top</span> <BsArrowUp/>
       </motion.div>
     )
